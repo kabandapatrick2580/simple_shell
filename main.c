e36a5a90210c26bd3e1d434c574ff1b809f721e2
kabandapatrick2580 <sendsome2019@gmail.com>
2023-04-20 10:44:16 -0700

    Shell created

--- main.c
+++ main.c
@@ -13,9 +13,8 @@ int main(int ac, char **argv)
 	char *prompt = "(simple_shell) $";
 	char *lineptr;
 	size_t n = 0;
-
 	(void)ac;
-      	(void)argv;
+	(void)argv;
 	printf("%s", prompt);
 	getline(&lineptr, &n, stdin);
 	printf("%s\n", lineptr);
