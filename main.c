fb538117578cadcd7d3f7f48c85c859a80866e85
kabandapatrick2580 <sendsome2019@gmail.com>
2023-04-20 09:47:52 -0700

    Boilerplate

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print bolilerplate
+ *
+ * @argv: argument value
+ * @ac: count
+ * Description: description
+ * Return: 0 always success
+ */
+int main(int ac, char **argv)
+{
+	return (0);
+}
