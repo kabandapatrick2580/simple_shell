24ce117b20cfdc18255931ed128d6cdb2ee197b3
kabandapatrick2580 <sendsome2019@gmail.com>
2023-04-27 12:54:50 -0700

    errors.c and errors1.c

--- errors.c
+++ errors.c
@@ -1,79 +1,83 @@
 #include "simpleshell.h"
+
 /**
- * _eputs - prints an input string
+ *_eputs - prints an input string
  * @str: the string to be printed
  *
- * Return: nothing
+ * Return: Nothing
  */
-void_eputs(char *str)
+void _eputs(char *str)
 {
 	int i = 0;
 
-	if(!str)
+	if (!str)
 		return;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_eputchar(str[i]);
 		i++;
 	}
 }
+
 /**
  * _eputchar - writes the character c to stderr
- * @c: the character to print
+ * @c: The character to print
  *
- * Return: on success 1.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int_eputchar(char c)
+int _eputchar(char c)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if(c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(2, buf, i);
 		i = 0;
 	}
-	if(c != BUF_FLUSH)
+	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
 }
+
 /**
  * _putfd - writes the character c to given fd
- * @c: the character to print
- * @fd: the file descriptor to write to
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
  *
- * Return: on success 1.
- * On error, -1 is is returned, and errno is set appropriately.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int_putfd(char c, int fd)
+int _putfd(char c, int fd)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if(c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(fd, buf, i);
 		i = 0;
 	}
-	if(c != BUF_FLUSH)
+	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
 }
+
 /**
- * _putsfd - prints an input string
+ *_putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the file descripor to write to
+ * @fd: the filedescriptor to write to
  *
  * Return: the number of chars put
  */
-int_putsfd(char *str, int fd)
+int _putsfd(char *str, int fd)
 {
 	int i = 0;
 
-	if(!str)
+	if (!str)
 		return (0);
-	while(*str)
+	while (*str)
 	{
 		i += _putfd(*str++, fd);
 	}
